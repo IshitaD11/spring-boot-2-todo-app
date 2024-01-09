8f0bdbf90f22fbff47823e6d4c0d637e0c87218c
Ishita Das <ishita.das9038@gmail.com>
2024-01-09 22:11:43 +0530

    adding project to repo

---
+++ spring-boot-2-todo-app/src/main/java/com/ishita/springboot2todoapp/SpringBoot2TodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.ishita.springboot2todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2TodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2TodoAppApplication.class, args);
+	}
+
+}
---
+++ spring-boot-2-todo-app/src/test/java/com/ishita/springboot2todoapp/SpringBoot2TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishita.springboot2todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
